a628d79ff8a2fd5b37442731a22c147809e5b98b
melih <melih16.meral@gmail.com>
2022-08-31 14:04:08 +0300

    Çoklu Dil Desteği düzenlemesi.

--- CrmBox.WebUI/Controllers/ChatController.cs
+++ CrmBox.WebUI/Controllers/ChatController.cs
@@ -1,6 +1,8 @@
-﻿using CrmBox.Persistance.Context;
+﻿using CrmBox.Core.Domain.Identity;
+using CrmBox.Persistance.Context;
 using CrmBox.WebUI.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
@@ -11,10 +13,12 @@ namespace CrmBox.WebUI.Controllers
     public class ChatController : Controller
     {
         private readonly CrmBoxIdentityContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public ChatController(CrmBoxIdentityContext context)
+        public ChatController(CrmBoxIdentityContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()    
@@ -28,13 +32,13 @@ namespace CrmBox.WebUI.Controllers
         public IActionResult Chat()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            
+            var userName = _userManager.Users.FirstOrDefault(x=>x.Id.ToString()==userId).UserName;
             ChatVM chatVm = new()
             {
                 
-                MaxRoomAllowed = 4,
                 UserId = userId,
             };
+            ViewBag.userName = userName;
             return View(chatVm);
         }
 
