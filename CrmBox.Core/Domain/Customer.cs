f94956352d10aad5a733a3be0d2f5a87a20c3537
melih <melih16.meral@gmail.com>
2022-09-06 15:18:16 +0300

    Güncellemeler eklendi.

--- CrmBox.Core/Domain/Customer.cs
+++ CrmBox.Core/Domain/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using CrmBox.Core.Domain.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace CrmBox.Core.Domain;
 
@@ -6,6 +7,8 @@ public class Customer : BaseEntity
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    [Required(ErrorMessage = "Mobile Number is required.")]
+    [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
     public string Address { get; set; }
--- CrmBox.WebUI/ValidationRules/CustomerValidation.cs
+++ CrmBox.WebUI/ValidationRules/CustomerValidation.cs
@@ -5,11 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Twilio.TwiML.Voice;
 
 namespace CrmBox.Application.Services.Customer
 {
-    public class CustomerValidation: AbstractValidator<AddCustomerVM> 
+    public class CustomerValidation : AbstractValidator<AddCustomerVM>
     {
         public CustomerValidation()
         {
@@ -37,6 +39,19 @@ namespace CrmBox.Application.Services.Customer
             RuleFor(x => x.LastName).MaximumLength(30);
             RuleFor(x => x.CompanyName).MaximumLength(20);
             RuleFor(x => x.JobTitle).MaximumLength(15);
+
+         
+
+
         }
+        private bool IsPhoneNumberMasking(string arg)
+        {
+            // Proje ilerki safhalarda kullanılabilir. Must komutu içinde çağırılıp.
+            Regex regex = new Regex(@"([\(? (\d{0,3})\)?\s]+[-\s])(?=\d{4})");
+            return regex.IsMatch(arg);
+        }
+
+
     }
+
 }
