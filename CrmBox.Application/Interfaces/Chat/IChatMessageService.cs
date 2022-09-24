129d8f727c7ad223144bcece64c0b778f2831036
melih <melih16.meral@gmail.com>
2022-09-24 21:54:54 +0300

    chatMessageServiceAdded.

---
+++ CrmBox.Application/Interfaces/Chat/IChatMessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrmBox.Core.Domain;
+
+namespace CrmBox.Application.Interfaces.Chat
+{
+    public interface IChatMessageService:IGenericService<ChatMessage>
+    {
+
+    }
+}
---
+++ CrmBox.Application/Services/Chat/ChatMessageService.cs
@@ -0,0 +1,19 @@
+﻿using CrmBox.Application.Interfaces.Customer;
+using CrmBox.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrmBox.Application.Interfaces.Chat;
+using CrmBox.Core.Domain;
+
+namespace CrmBox.Application.Services.Chat
+{
+    public class ChatMessageService : GenericService<ChatMessage, CrmBoxIdentityContext>, IChatMessageService
+    {
+        public ChatMessageService(CrmBoxIdentityContext context) : base(context)
+        {
+        }
+    }
+}
